d4b6de80951c725deb747560bcb9363063ad7135
Dawon Lee <109711119+Dawonlee0@users.noreply.github.com>
2024-09-29 03:32:27 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "Dawonlee",
     image: "avatar1.svg", 
     // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "developer!",
+    role: "developer",
     bio: "I am dawon",
     email: "ttoyr219@naver.com",
     linkedin: "",
