d9c7208b058f88b7edd1f6b91e0b6eba50559ea0
Dawon Lee <109711119+Dawonlee0@users.noreply.github.com>
2024-11-25 12:38:26 +0900

    fix : 배경

--- src/routes/Feed/index.tsx
+++ src/routes/Feed/index.tsx
@@ -11,6 +11,7 @@ import ServiceCard from "./ServiceCard"
 import ContactCard from "./ContactCard"
 import PostList from "./PostList"
 import PinnedPosts from "./PostList/PinnedPosts"
+import { css } from "@emotion/react"
 
 const HEADER_HEIGHT = 73
 
@@ -20,7 +21,7 @@ const Feed: React.FC<Props> = () => {
   const [q, setQ] = useState("")
 
   return (
-    <StyledWrapper>
+    <>
       <div className="ocean-banner">
         <Image
           src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?fm=jpg&q=60&w=3000"
@@ -30,134 +31,66 @@ const Feed: React.FC<Props> = () => {
           priority
         />
       </div>
-      <div
-        className="lt"
-        css={{
-          height: `calc(100vh - ${HEADER_HEIGHT}px)`,
-        }}
-      >
-        <TagList />
-      </div>
-      <div className="mid">
-        <MobileProfileCard />
-        <PinnedPosts q={q} />
-        <SearchInput value={q} onChange={(e) => setQ(e.target.value)} />
-        <div className="tags">
+      <StyledWrapper>
+        <div
+          className="lt"
+          css={{
+            height: `calc(100vh - ${HEADER_HEIGHT}px)`,
+          }}
+        >
           <TagList />
         </div>
-        <FeedHeader />
-        <PostList q={q} />
-        <div className="footer">
-          <Footer />
+        <div className="mid">
+          <MobileProfileCard />
+          <PinnedPosts q={q} />
+          <SearchInput value={q} onChange={(e) => setQ(e.target.value)} />
+          <div className="tags">
+            <TagList />
+          </div>
+          <FeedHeader />
+          <PostList q={q} />
+          <div className="footer">
+            <Footer />
+          </div>
         </div>
-      </div>
-      <div
-        className="rt"
-        css={{
-          height: `calc(100vh - ${HEADER_HEIGHT}px)`,
-        }}
-      >
-        <ProfileCard />
-        <ServiceCard />
-        <ContactCard />
-        <div className="footer">
-          <Footer />
+        <div
+          className="rt"
+          css={{
+            height: `calc(100vh - ${HEADER_HEIGHT}px)`,
+          }}
+        >
+          <ProfileCard />
+          <ServiceCard />
+          <ContactCard />
+          <div className="footer">
+            <Footer />
+          </div>
         </div>
-      </div>
-    </StyledWrapper>
+      </StyledWrapper>
+    </>
   )
 }
 
 export default Feed
 
 const StyledWrapper = styled.div`
+  grid-template-columns: repeat(12, minmax(0, 1fr));
+  padding: 2rem 0;
+  display: grid;
+  gap: 1.5rem;
+`
+
+const globalStyles = css`
   .ocean-banner {
     position: relative;
     width: 100%;
     height: 400px;
     margin-bottom: 2rem;
-    border-radius: 1.5rem;
+    border-radius: 0;
     overflow: hidden;
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-      0 2px 4px -1px rgba(0, 0, 0, 0.06);
-  }
-
-  grid-template-columns: repeat(12, minmax(0, 1fr));
-  padding: 2rem 0;
-  display: grid;
-  gap: 1.5rem;
-
-  @media (max-width: 768px) {
-    display: block;
-    padding: 0.5rem 0;
     
-    .ocean-banner {
+    @media (max-width: 768px) {
       height: 200px;
-      border-radius: 1rem;
-      margin-bottom: 1rem;
-    }
-  }
-
-  > .lt {
-    display: none;
-    overflow: scroll;
-    position: sticky;
-    grid-column: span 2 / span 2;
-    top: ${HEADER_HEIGHT - 10}px;
-
-    scrollbar-width: none;
-    -ms-overflow-style: none;
-    &::-webkit-scrollbar {
-      display: none;
-    }
-
-    @media (min-width: 1024px) {
-      display: block;
-    }
-  }
-
-  > .mid {
-    grid-column: span 12 / span 12;
-
-    @media (min-width: 1024px) {
-      grid-column: span 7 / span 7;
-    }
-
-    > .tags {
-      display: block;
-
-      @media (min-width: 1024px) {
-        display: none;
-      }
-    }
-
-    > .footer {
-      padding-bottom: 2rem;
-      @media (min-width: 1024px) {
-        display: none;
-      }
-    }
-  }
-
-  > .rt {
-    scrollbar-width: none;
-    -ms-overflow-style: none;
-    &::-webkit-scrollbar {
-      display: none;
-    }
-
-    display: none;
-    overflow: scroll;
-    position: sticky;
-    top: ${HEADER_HEIGHT - 10}px;
-
-    @media (min-width: 1024px) {
-      display: block;
-      grid-column: span 3 / span 3;
-    }
-
-    .footer {
-      padding-top: 1rem;
     }
   }
 `
