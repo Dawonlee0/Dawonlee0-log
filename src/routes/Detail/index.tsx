4520817a1c34e282202a5082c445dc14a3a05376
Dawon Lee <109711119+Dawonlee0@users.noreply.github.com>
2024-11-26 15:44:32 +0900

    feat : 플로팅 목차

--- src/routes/Detail/index.tsx
+++ src/routes/Detail/index.tsx
@@ -3,33 +3,24 @@ import PostDetail from "./PostDetail"
 import PageDetail from "./PageDetail"
 import styled from "@emotion/styled"
 import usePostQuery from "src/hooks/usePostQuery"
-import { useRouter } from "next/router"
+import TableOfContents from "./components/TableOfContents"
 
 type Props = {}
 
 const Detail: React.FC<Props> = () => {
   const data = usePostQuery()
-  const router = useRouter()
   useMermaidEffect()
-
-  const handleBackgroundClick = (e: React.MouseEvent) => {
-    const clickedElement = e.target as HTMLElement;
-    if (!clickedElement.closest('.container') && clickedElement.classList.contains('clickable-area')) {
-      router.back();
-    }
-  }
 
   if (!data) return null
   return (
-    <StyledWrapper onClick={handleBackgroundClick}>
-      <div className="clickable-area left" />
+    <StyledWrapper>
       <div className="container">
         <div className="content" data-type={data.type}>
           {data.type[0] === "Page" && <PageDetail />}
           {data.type[0] !== "Page" && <PostDetail />}
         </div>
       </div>
-      <div className="clickable-area right" />
+      <TableOfContents />
     </StyledWrapper>
   )
 }
@@ -40,19 +31,11 @@ const StyledWrapper = styled.div`
   min-height: 100vh;
   padding: 2rem 0;
   background-color: ${({ theme }) => theme.colors.gray2};
-  display: flex;
-
-  .clickable-area {
-    flex: 1;
-    cursor: pointer;
-    min-width: 50px;
-  }
 
   .container {
     width: 100%;
     max-width: 60rem;
     margin: 0 auto;
-    flex-shrink: 0;
   }
 
   .content {
@@ -69,7 +52,6 @@ const StyledWrapper = styled.div`
     }
   }
 
-  /** Reference: https://github.com/chriskempson/tomorrow-theme **/
   code[class*="language-mermaid"],
   pre[class*="language-mermaid"] {
     background-color: ${({ theme }) => theme.colors.gray5};
