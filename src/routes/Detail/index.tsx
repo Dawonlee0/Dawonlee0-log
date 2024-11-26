51b4138e05da1fb54317b51b8479544cee7b99b3
Dawon Lee <109711119+Dawonlee0@users.noreply.github.com>
2024-11-26 15:17:45 +0900

    feat : +

--- src/routes/Detail/index.tsx
+++ src/routes/Detail/index.tsx
@@ -3,18 +3,28 @@ import PostDetail from "./PostDetail"
 import PageDetail from "./PageDetail"
 import styled from "@emotion/styled"
 import usePostQuery from "src/hooks/usePostQuery"
+import { useRouter } from "next/router"
 
 type Props = {}
 
 const Detail: React.FC<Props> = () => {
   const data = usePostQuery()
+  const router = useRouter()
   useMermaidEffect()
+
+  const handleBackgroundClick = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      router.back()
+    }
+  }
 
   if (!data) return null
   return (
-    <StyledWrapper data-type={data.type}>
-      {data.type[0] === "Page" && <PageDetail />}
-      {data.type[0] !== "Page" && <PostDetail />}
+    <StyledWrapper onClick={handleBackgroundClick}>
+      <div className="content" data-type={data.type}>
+        {data.type[0] === "Page" && <PageDetail />}
+        {data.type[0] !== "Page" && <PostDetail />}
+      </div>
     </StyledWrapper>
   )
 }
@@ -22,11 +32,26 @@ const Detail: React.FC<Props> = () => {
 export default Detail
 
 const StyledWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   padding: 2rem 0;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.colors.gray2};
 
-  &[data-type="Paper"] {
-    padding: 40px 0;
+  .content {
+    position: relative;
+    max-width: 60rem;
+    margin: 0 auto;
+    cursor: default;
+    
+    &[data-type="Paper"] {
+      padding: 40px 0;
+    }
   }
+
   /** Reference: https://github.com/chriskempson/tomorrow-theme **/
   code[class*="language-mermaid"],
   pre[class*="language-mermaid"] {
