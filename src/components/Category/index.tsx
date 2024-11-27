7eb7b1c5e7c3447945fd8386b5b175c5b9f2dcd9
Dawon Lee <109711119+Dawonlee0@users.noreply.github.com>
2024-11-28 05:48:15 +0900

    refactor: replace hardcoded color set with imported pastel colors in Category component

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,18 +1,7 @@
 import { useRouter } from "next/router"
 import React, { useMemo } from "react"
 import styled from "@emotion/styled"
-
-// 미리 정의된 파스텔톤 색상 세트
-const PASTEL_COLORS = [
-  "hsl(190, 90%, 85%)",  // 하늘색
-  "hsl(350, 90%, 85%)",  // 분홍색
-  "hsl(280, 90%, 85%)",  // 보라색
-  "hsl(220, 90%, 85%)",  // 파란색
-  "hsl(160, 90%, 85%)",  // 민트색
-  "hsl(120, 90%, 85%)",  // 연두색
-  "hsl(45, 90%, 85%)",   // 노란색
-  "hsl(20, 90%, 85%)",   // 주황색
-]
+import { PASTEL_COLORS } from './constants'
 
 // 수정: 색상 생성 함수를 캐시로 관리
 const colorCache: { [key: string]: string } = {}
